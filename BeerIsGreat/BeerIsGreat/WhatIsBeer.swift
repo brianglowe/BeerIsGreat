3fb7dc69cd23b6373a7692442d2f75d966c0e429
Brian Glowe <brianglowe@gmail.com>
2015-10-12 21:52:59 -0400

    Created WhatIsBeer Struct

---
+++ BeerIsGreat/BeerIsGreat/WhatIsBeer.swift
@@ -0,0 +1,48 @@
+//
+//  WhatIsBeer.swift
+//  BeerIsGreat
+//
+//  Created by Brian J Glowe on 10/12/15.
+//  Copyright © 2015 Brian Glowe. All rights reserved.
+//
+
+import Foundation
+
+struct WhatIsBeer {
+    
+    let ingredients = [
+        [
+            "title": "Malt",
+            "description": "Malt contributes to the flavor, color, and mouthfeel of a beer. To create malt, a grain is soaked in water until it begins to sprout, converting its starch reserves into sugars.  This process is halted by drying the grains in a kiln, providing a source of sugar and soluble starch for the fermentation process.",
+            "icon": "coffee.pdf",
+            "largeIcon": "coffee_large.pdf",
+            "backgroundColor": ["red": 255, "green": 204, "blue": 51, "alpha": 1.0],
+         //   "artists": ["American Authors", "Vacationer", "Matt and Kim", "MGMT", "Echosmith", "Tokyo Police Club", "La Femme"]
+        ],
+        [
+            "title": "Hops",
+            "description": "Hops add the much needed (and for some preferred) bitterness that balances the sweetness that remains in beer after the fermentation process ends.",
+            "icon": "coffee.pdf",
+            "largeIcon": "coffee_large.pdf",
+            "backgroundColor": ["red": 255, "green": 204, "blue": 51, "alpha": 1.0],
+            //   "artists": ["American Authors", "Vacationer", "Matt and Kim", "MGMT", "Echosmith", "Tokyo Police Club", "La Femme"]
+        ],
+        [
+            "title": "Water",
+            "description": "Beer is 95% water so it is to no surprise historically breweries strategically located around high quality water sources.",
+            "icon": "coffee.pdf",
+            "largeIcon": "coffee_large.pdf",
+            "backgroundColor": ["red": 255, "green": 204, "blue": 51, "alpha": 1.0],
+            //   "artists": ["American Authors", "Vacationer", "Matt and Kim", "MGMT", "Echosmith", "Tokyo Police Club", "La Femme"]
+        ],
+        [
+            "title": "Yeast",
+            "description": "The single-cell organisms are the magic makers of the brewing process.  Yeast consumed the sugar from the malt to produce alcohol and carbon dioxide during fermentation.",
+            "icon": "coffee.pdf",
+            "largeIcon": "coffee_large.pdf",
+            "backgroundColor": ["red": 255, "green": 204, "blue": 51, "alpha": 1.0],
+            //   "artists": ["American Authors", "Vacationer", "Matt and Kim", "MGMT", "Echosmith", "Tokyo Police Club", "La Femme"]
+        ],
+    ]
+
+}
